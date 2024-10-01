7db93809a37a15809bb0984769755565166567ea
Bryan <bryanonyen.inc@gmail.com>
2024-10-01 18:23:33 +0100

    services page error fix

--- src/app/seo/page.tsx
+++ src/app/seo/page.tsx
@@ -1,7 +1,7 @@
 import SeoContact from '@/components/SeoContact'
 import React,{ Suspense } from 'react'
 
-export const page = () => {
+export const SeoContactPage = () => {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
